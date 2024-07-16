7ef703f4fc6322464b9b028bad6a59a5a487e53c
kk <kirill.kostenkov@homagames.com>
2024-07-16 15:16:18 +0200

    cd .. before installing jb

--- src/installer.ts
+++ src/installer.ts
@@ -4,7 +4,7 @@ export class Installer {
   //TODO check dotnet sdk in constructor
   async install(version: string): Promise<number> {
     // The 'update' command will install JetBrains.ReSharper.GlobalTools if it is not already installed.
-    let command = `dotnet tool update --global JetBrains.ReSharper.GlobalTools`
+    let command = `cd ..; dotnet tool update --global JetBrains.ReSharper.GlobalTools`
 
     if (version !== '') {
       command += ` --version ${version}`
